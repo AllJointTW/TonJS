315a9b5d52c1d687c657c08f3bf5fdc7ea55a778
Allen Hsieh <allen804337@gmail.com>
2020-05-12 19:31:04 +0800

    fix(vuepress): base url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'TonJS',
   description: 'The Node.js Web Framework Can Do A Ton Of Request',
-  base: '/test/',
+  base: '/',
   repo: 'https://github.com/AllJointTW/TonJS',
   themeConfig: {
     sidebarDepth: 0,
