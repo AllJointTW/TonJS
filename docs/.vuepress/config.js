cbd2fab610eaca0ea3af03d67ff81e1060407356
Allen Hsieh <allen804337@gmail.com>
2020-05-12 18:52:23 +0800

    feat(vuepress): init document

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'TonJS',
+  description: 'The Node.js Web Framework Can Do A Ton Of Request',
+  base: '/test/',
+  repo: 'https://github.com/AllJointTW/TonJS',
+  themeConfig: {
+    sidebarDepth: 0,
+    sidebar: [
+      ['/']
+    ],
+    nav: [
+      { text: 'Home', link: '/' },
+    ]
+  }
+}
