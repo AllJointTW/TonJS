bbfc516d62bba289be487ccbce7d8bfabe1fb175
Allen Hsieh <allen804337@gmail.com>
2020-05-13 19:15:17 +0800

    chore(static): write example code

--- example/manual.ts
+++ example/manual.ts
@@ -7,16 +7,20 @@ import {
   route
 } from '../packages/ton/src'
 import * as logger from '../packages/logger'
+import { createStatic } from '../packages/static/src'
 
 const host = '0.0.0.0'
 const port = 3000
 
 async function main() {
   const app = createApp()
+  const staticServer = createStatic({})
   route(app, 'get', '/', (req, res) => send(res, 200, 'TonJS'))
   route(app, 'get', '/redirect', (req, res) =>
     redirect(res, 302, 'https://tonjs.com')
   )
+  route(app, 'get', '/public/*', staticServer())
+
   const token = await listen(app, host, port)
   registerGracefulShutdown(token)
   logger.info(`\nyou raise me up, to listen on http://${host}:${port}\n`)
