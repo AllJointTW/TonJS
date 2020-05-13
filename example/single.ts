eddac12f1f0639a5fab115bf9ec41895697a5afc
TZU-YEN, CHANG <try_love_tom@alljoint.tw>
2020-05-13 17:11:28 +0800

    docs(cors): add the example about it

--- example/single.ts
+++ example/single.ts
@@ -1,22 +1,4 @@
-// import cors from '../packages/cors/src'
-import { TonHandler, sendEmpty } from '../packages/ton/dist'
+import { TonHandler } from '../packages/ton'
 
-const yourHandler = () => 'Hi There!'
-
-function cors(handler: TonHandler) {
-  const wrapper: TonHandler = (req, res) => {
-    console.log(req.getMethod())
-    res.writeHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8887')
-    if (req.getMethod() === 'OPTIONS') {
-      res.writeHeader('Access-Control-Max-Age', String(60 * 60 * 24))
-      sendEmpty(res)
-    } else {
-      return handler(req, res)
-    }
-  }
-  return wrapper
-}
-
-const singleWithCors = cors(yourHandler)
-
-export default singleWithCors
+const single: TonHandler = () => ''
+export default single
