1300102c123cfc1a2f8a900c1faca1de50be310f
TZU-YEN, CHANG <try_love_tom@alljoint.tw>
2020-05-16 18:05:08 +0800

    docs: recomment the deploy.sh and rename to deploy-docs.sh

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
 
-# 當發生錯誤時中止腳本
+# exit immediately if a command exits with a non-zero status
 set -e
 
-# 構建
-npm run docs:build
+# build the docs
+yarn docs:build
 
-# cd 到構建輸出的目錄下
+# cd to the output
 cd docs/.vuepress/dist
 
-# 部署到自定義域域名
+# setup the cname
 echo 'tonjs.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io
+# deploy to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
+# deploy to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:AllJointTW/TonJS.git master:gh-pages
 
 cd -
