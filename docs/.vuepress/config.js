4f3afe3b95a4e165e4da7491fd0842e615d23abe
Allen <48684662+Allen804347@users.noreply.github.com>
2020-05-16 18:10:20 +0800

    docs: add vuepress to the project (#29)
    * chore(vuepress): add vuepress to project
    
    * chore(vuepress): add script to package
    
    * feat(vuepress): init document
    
    * chore(vuepress): setup deploy to github page
    
    * fix(vuepress): deploy config
    
    * fix(vuepress): base url
    
    * fix(vuepress): restore cname
    
    * docs(cors): add first version documentation
    
    * docs(vuepress): content style enahnce
    
    * docs: recomment the deploy.sh and rename to deploy-docs.sh
    
    Co-authored-by: Allen <Allen804347@gmail.com>
    Co-authored-by: TZU-YEN, CHANG <try_love_tom@alljoint.tw>

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'TonJS',
+  description: 'The Node.js Web Framework Can Do A Ton Of Request',
+  base: '/',
+  repo: 'https://github.com/AllJointTW/TonJS',
+  themeConfig: {
+    sidebarDepth: 0,
+    sidebar: [
+      '/',
+      '/cors/'
+    ],
+    nav: [
+      { text: 'Home', link: '/' },
+    ]
+  }
+}
