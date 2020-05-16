748cc435b041cccec5e6beea2eb645ab47c8bd3c
TZU-YEN, CHANG <try_love_tom@alljoint.tw>
2020-05-16 16:48:07 +0800

    refactor(upload): readFileStream and createBusboy

--- packages/upload/src/index.ts
+++ packages/upload/src/index.ts
@@ -1,8 +1,8 @@
-import { Readable, PassThrough } from 'stream'
+import { Readable, PassThrough, pipeline } from 'stream'
 import Busboy from 'busboy'
-import { TonRequest, TonResponse, readStream } from '@tonjs/ton/src/index'
+import { TonRequest, TonResponse, readStream } from '@tonjs/ton'
 
-export function createBusboy(options: busboy.BusboyConfig) {
+export function createBusboy(options: busboy.BusboyConfig = {}) {
   try {
     return new Busboy(options)
   } catch (error) {
@@ -14,38 +14,54 @@ export function createBusboy(options: busboy.BusboyConfig) {
   }
 }
 
-export function readFile(
+export function readFileStream(
   req: TonRequest,
   res: TonResponse,
-  options: { limit?: string }
+  options?: { limit?: string }
 ): Promise<{
-  field: string
-  stream: Readable
-  name: string
-  encoding: string
-  mime: string
+  field?: string
+  stream?: Readable
+  name?: string
+  encoding?: string
+  mime?: string
 }> {
-  return new Promise((resolve, reject) => {
-    const headers = {}
-    req.forEach((key, value) => {
-      headers[key] = value
-    })
+  return new Promise(resolve => {
+    const headers = {
+      'content-type': req.getHeader('content-type')
+    }
+    const busboy = createBusboy({ headers, limits: { files: 1 } })
 
-    const busboy = new Busboy({ headers, limits: { files: 1 } })
-    readStream(req, res, options)
-      .on('error', reject)
-      .pipe(busboy)
-      .on(
-        'file',
-        (
-          field: string,
-          stream: Readable,
-          name: string,
-          encoding: string,
-          mime: string
-        ) => {
-          resolve({ field, stream, name, encoding, mime })
-        }
-      )
+    busboy.on(
+      'file',
+      (
+        field: string,
+        stream: Readable,
+        name: string,
+        encoding: string,
+        mime: string
+      ) => {
+        resolve({ field, stream, name, encoding, mime })
+      }
+    )
+
+    pipeline(readStream(req, res, options), busboy, err => {
+      const errorEmitter = new PassThrough()
+
+      if (err) {
+        Promise.resolve().then(() => {
+          process.nextTick(() => {
+            errorEmitter.emit('error', err)
+          })
+        })
+      }
+
+      resolve({
+        field: undefined,
+        stream: errorEmitter,
+        name: undefined,
+        encoding: undefined,
+        mime: undefined
+      })
+    })
   })
 }
