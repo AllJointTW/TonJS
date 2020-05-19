a2119334fdf00c955f0ddb188fd193c2551c77a7
TZU-YEN, CHANG <try_love_tom@alljoint.tw>
2020-05-19 18:08:37 +0800

    chore(compress): setup the compress module

--- example/sendFile.ts
+++ example/sendFile.ts
@@ -1,12 +1,17 @@
 import { join } from 'path'
 import { createReadStream, statSync } from 'fs'
 import { TonHandler, TonStream, sendStream } from '../packages/ton'
+// import { sendCompressedStream } from '../packages/compress'
 
 const target = join(__dirname, 'public/sample.jpg')
 const { size } = statSync(target)
 const sendFile: TonHandler = (req, res) => {
   const stream: TonStream = createReadStream(target)
   stream.size = size
+  // need compress
+  // sendCompressedStream(req, res, 200, stream)
+
+  // just send
   sendStream(res, 200, stream)
 }
 export default sendFile
