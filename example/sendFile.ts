ad23bb493c18311b0554ccc0d66e62a54570a57c
Allen <Allen804337@gmail.com>
2020-05-20 19:36:59 +0800

    docs(ton): add some comments

--- example/sendFile.ts
+++ example/sendFile.ts
@@ -6,6 +6,11 @@ const target = join(__dirname, 'public/sample.jpg')
 const { size } = statSync(target)
 const sendFile: TonHandler = (req, res) => {
   const stream: TonStream = createReadStream(target)
+  // stream.size can be undefined
+  // if stream.size === undefined, it will use res.write
+  // if stream.size has been set, it will use res.tryEnd
+  // and `res.tryEnd` is faster than `res.write`
+  // reference: https://github.com/uNetworking/uWebSockets.js/issues/209
   stream.size = size
   sendStream(res, 200, stream)
 }
